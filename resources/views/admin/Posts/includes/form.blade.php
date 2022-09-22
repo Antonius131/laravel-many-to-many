dcd5535c78d676ac967ad4380d0ef93ce7fc44b9
Antonius131 <antomendetta@gmail.com>
2022-09-22 18:01:27 +0200

    put tag checkboxes in form

--- resources/views/admin/Posts/includes/form.blade.php
+++ resources/views/admin/Posts/includes/form.blade.php
@@ -9,10 +9,20 @@
       <p>Author: <strong>{{ Auth::user()->name }}</strong></p>
    </div>
 @endif
+
 <div class="mb-3">
    <label for="input-title" class="form-label">Title</label>
    <input type="text" name="title" class="form-control" id="input-title"
    value="{{ request()->routeIs('admin.posts.edit') ? $post->title : '' }}">
+</div>
+<div class="mb-3 form-check">
+   @foreach ($tags as $tag)   
+      <input type="checkbox" name="tags[]" class="form-check-input" id="input-tags"
+      value="">
+      <label for="input-tags" class="form-check-label">
+         {{ $tag->tag_name }}
+      </label>
+   @endforeach
 </div>
 <div class="mb-3">
    <label class="form-label" for="create-textarea">Post text</label>
