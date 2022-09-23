78804e3e8af7beb4785fb0f0b0d7a010778d6779
Antonius131 <antomendetta@gmail.com>
2022-09-23 15:27:41 +0200

    showing posts related to tag

--- resources/views/admin/tags/show.blade.php
+++ resources/views/admin/tags/show.blade.php
@@ -7,12 +7,25 @@
          <div class="row">
             <div class="col-12">
                <h3>Tag: {{ $tag->tag_name }}</h3>
-               {{-- <p class="mt-2">
-                  @foreach ( $post as $post)
-                     {{ $tag->tag_name }}
-                  @endforeach
-               </p> --}}
             </div>
+         </div>
+         <div class="row py-5">
+            @forelse ($tag->posts as $post)
+               <div class="col-6 mb-3">
+                  <div class="card">
+                     <div class="card-body">
+                        <h6 class="card-title">
+                           {{ $post->title }}
+                        </h6>
+                        <a href="{{ route('admin.posts.show', $post->id) }}" class="btn-link">
+                           Go to post
+                        </a>
+                     </div>
+                  </div>
+               </div>
+            @empty
+               <h2>No posts assigned to this Tag</h2>
+            @endforelse
          </div>
       </div>
    </div>
