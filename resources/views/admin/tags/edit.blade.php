869c4bb36e5ff18bb1f0b2e99ebf6540afbee2e1
Antonius131 <antomendetta@gmail.com>
2022-09-23 16:21:07 +0200

    fixing route and include methods

--- resources/views/admin/tags/edit.blade.php
+++ resources/views/admin/tags/edit.blade.php
@@ -4,11 +4,11 @@
    <div class="container">
       <div class="row py-5">
          <div class="col-8 offset-2">
-            <form action="{{ route('admin.posts.update', $post->id) }}" method="POST">
+            <form action="{{ route('admin.tags.update', $tags->id) }}" method="POST">
                @csrf
                @method('PUT')
-               @include('admin.Posts.includes.form')
-               <button type="submit" class="btn btn-primary">Edit post</button>
+               @include('admin.tags.includes.form')
+               <button type="submit" class="btn btn-primary">Edit tag</button>
             </form>
          </div>
       </div>
