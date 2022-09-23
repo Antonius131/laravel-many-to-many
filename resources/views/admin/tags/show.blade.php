248feb8781b699028ee6f99aa47244f5055fb418
Antonius131 <antomendetta@gmail.com>
2022-09-23 15:01:08 +0200

    creating new show.blade

---
+++ resources/views/admin/tags/show.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+   <div class="row py-5">
+      <div class="col-8 offset-2">
+         <div class="row">
+            <div class="col-12">
+               <h6>Author: {{ $post->user->name }}</h6>
+               <h1>{{ $post->title }}</h1>
+               <small>{{ $post->date }}</small>
+               <p class="mt-2">
+                  @foreach ( $post->tags as $tag)
+                     #{{ $tag->tag_name }} &nbsp
+                  @endforeach
+               </p>
+            </div>
+         </div>
+         <div class="row mt-4">
+            <div class="col-12">
+               <p>{{ $post->article_text }}</p>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+@endsection
