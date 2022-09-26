00daac8b8d3a17ef68ac47c53160a536a12df1c8
Antonius131 <antomendetta@gmail.com>
2022-09-26 15:00:01 +0200

    set condition to show post image

--- resources/views/admin/Posts/show.blade.php
+++ resources/views/admin/Posts/show.blade.php
@@ -8,7 +8,11 @@
             <div class="col-12">
                <h6>Author: {{ $post->user->name }}</h6>
                <h1>{{ $post->title }}</h1>
-               <img src="{{ $post->post_image }}" class="img-fluid w-100 mb-2" alt="{{ $post->title }}">
+               @if (Storage::disk()->exists($post->post_image))
+               <img src="{{ asset('storage/' . $post->post_image) }}" class="img-fluid w-100 mb-2" alt="{{ $post->title }}">    
+                  @else
+                     <img src="{{ $post->post_image }}" class="img-fluid w-100 mb-2" alt="{{ $post->title }}">
+               @endif
                <small>Date: {{ $post->date }}</small>
                <p class="mt-2">
                   @foreach ( $post->tags as $tag)
